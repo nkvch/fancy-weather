0db0354cc81fe42eac4b82b7ffe64da82894493e
Stanislau Stankevich <s.stankevich@stud.elka.pw.edu.pl>
2021-07-05 16:44:56 +0200

    proxies trust

--- api/location.js
+++ api/location.js
@@ -13,7 +13,7 @@ const ipinfo = new IPinfo(token);
 router.route('/')
 .get(async (req, res) => {
     try {
-        const ip = req.ip.split(':').pop();
+        const ip = req.ip;
         console.log(token);
         console.log(ip);
         console.log('tger');
