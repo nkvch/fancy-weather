3fe00a416b588bcb9ece61f0b09408811ea4ab6b
Stanislau Stankevich <s.stankevich@stud.elka.pw.edu.pl>
2021-07-05 17:19:13 +0200

    images added

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,29 @@
 import './styles/main.scss';
-import { ImageService } from './modules/imageService';
 
 const app = document.createElement('div');
 app.className = 'container-fluid app';
 document.body.appendChild(app);
+
+let city;
+let country;
+let loc;
+let timezone;
 
 // ImageService.searchWithQuery('warsaw')
 // .then(warsaw => {
 //     app.style.backgroundImage = `url('${warsaw.results[0].urls.full}')`;
 // });
 
+//When in production:
 fetch('/location')
+//fetch('https://super-fancy-weather.herokuapp.com/location')
 .then(async (res) => {
     const data = await res.json();
-    console.log('alala');
-    console.log(data);
+    city = data.city;
+    country = data.country;
+    loc = data.loc.split(',');
+    timezone = data.timezone;
+    console.log(city);
 })
 .catch((e) => {
     console.error(e);
